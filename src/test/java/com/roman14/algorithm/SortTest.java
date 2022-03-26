e6422b07e182f9a5ef77e3f354a69d135c996b80
= <roman14@naver.com>
2022-03-26 20:08:35 +0900

    프로젝트에 Gradle 추가, Sort 테스트케이스 파일 생성

---
+++ src/test/java/com/roman14/algorithm/SortTest.java
@@ -0,0 +1,30 @@
+package com.roman14.algorithm;
+
+
+import org.junit.jupiter.api.Test;
+
+class SortTest
+{
+  //org.hamcrest.MatcherAssert
+
+  @Test
+  void selectionSort()
+  {
+
+  }
+
+  @Test
+  void bubbleSort()
+  {
+  }
+
+  @Test
+  void insertionSort()
+  {
+  }
+
+  @Test
+  void quickSort()
+  {
+  }
+}
